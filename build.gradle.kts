d174954200fe980766590ff51b94a35811c76afa
Pavel Biryukov <birksovskiy@proton.me>
2024-01-02 19:52:53 +0300

    fix: add Logback adapter for SLF4J to fix related errors

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ repositories {
 
 dependencies {
     implementation("net.dv8tion:JDA:$jdaVersion")
+    implementation("ch.qos.logback:logback-classic:1.2.8")
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
